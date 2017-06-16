d87ed442cfa46e9422eff03c581b04c15861306f
FVI <test@examle.com>
2017-06-15 21:38:41 -0400

    until video 25 wess tutorial

---
+++ src/base.js
@@ -0,0 +1,8 @@
+import Rebase from 're-base';
+
+const base=Rebase.createClass({
+    apiKey: "AIzaSyDfSgUZDSW90p2jWez15SrhWBoDX9QEttg",
+    authDomain: "catch-of-the-day-lily.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-lily.firebaseio.com",
+});
+ export default base;
